62f236cae6b7da3358701d4d40811a59f7f43959
Himshikhar Gayan <hgayan@awesomemotive.com>
2024-06-05 15:06:05 +0530

    v0.0.5 changes

--- app/src/main/java/com/pushengage/PushNotificationDemo/PEApplication.java
+++ app/src/main/java/com/pushengage/PushNotificationDemo/PEApplication.java
@@ -12,11 +12,10 @@ public class PEApplication extends Application {
         super.onCreate();
 
         PEPrefs prefs = new PEPrefs(this);
-        prefs.setEnvironment(PEConstants.STG);
 
         PushEngage pushEngage = new PushEngage.Builder()
                 .addContext(getApplicationContext())
-                .setAppId("3ca8257d-1f40-41e0-88bc-ea28dc6495ef")
+                .setAppId("YOUR_APP_ID")
                 .build();
 
         PushEngage.enableLogging(true);
